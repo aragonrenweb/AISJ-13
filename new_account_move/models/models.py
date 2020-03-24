e5e77faf84c991f35a77b5aa75e5f6fdb7a38dc8
LuisMalave2001 <luisangelmalavemora@gmail.com>
2020-03-24 13:05:27 +0000

    Extension de modelo - Account_move

--- new_account_move/models/models.py
+++ new_account_move/models/models.py
@@ -1,18 +1,18 @@
- -*- coding: utf-8 -*-
+ #-*- coding: utf-8 -*-
 
  from odoo import models, fields, api
 
 
  class new_account_move(models.Model):
-     _name = 'new_account_move.new_account_move'
-     _description = 'new_account_move.new_account_move'
+     	_name = 'new_account_move.new_account_move'
+     	_description = 'new_account_move.new_account_move'
 
-     name = fields.Char()
-     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
-     description = fields.Text()
+     	name = fields.Char()
+     	value = fields.Integer()
+     	value2 = fields.Float(compute="_value_pc", store=True)
+	description = fields.Text()
 
-     @api.depends('value')
-     def _value_pc(self):
+	@api.depends('value')
+	def _value_pc(self):
          for record in self:
              record.value2 = float(record.value) / 100
