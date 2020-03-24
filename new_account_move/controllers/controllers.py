05f841b2b380a5d7f10f9725cecfe2590f1d2cad
DavidCondeCubas <dconde@eduwebgroup.com>
2020-03-24 12:42:08 +0000

    Extension de modelo - Account_move

---
+++ new_account_move/controllers/controllers.py
@@ -0,0 +1,21 @@
+ -*- coding: utf-8 -*-
+ from odoo import http
+
+
+ class NewAccountMove(http.Controller):
+     @http.route('/new_account_move/new_account_move/', auth='public')
+     def index(self, **kw):
+         return "Hello, world"
+
+     @http.route('/new_account_move/new_account_move/objects/', auth='public')
+     def list(self, **kw):
+         return http.request.render('new_account_move.listing', {
+             'root': '/new_account_move/new_account_move',
+             'objects': http.request.env['new_account_move.new_account_move'].search([]),
+         })
+
+     @http.route('/new_account_move/new_account_move/objects/<model("new_account_move.new_account_move"):obj>/', auth='public')
+     def object(self, obj, **kw):
+         return http.request.render('new_account_move.object', {
+             'object': obj
+         })
