5dfed173a7477d3b31b01f3b0a95153ce0c434b8
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2020-06-26 12:36:07 +0200

    feat(product): changed name to category id for more semantic naming

--- school_finance/models/product_product.py
+++ school_finance/models/product_product.py
@@ -4,9 +4,9 @@ from odoo import fields, models, api
 class ProductProduct (models.Model):
     _inherit = 'product.product'
 
-    facts_id = fields.Char("Fact id", related="categ_id.facts_id")
+    categ_facts_id = fields.Char("Fact id", related="categ_id.facts_id")
 
 class ProductTemplate (models.Model):
     _inherit = 'product.template'
 
-    facts_id = fields.Char("Fact id", related="categ_id.facts_id")
+    categ_facts_id = fields.Char("Fact id", related="categ_id.facts_id")
