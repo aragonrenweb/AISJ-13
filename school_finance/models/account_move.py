65078dff1a3572c8ead42b3d0aed6431d5ba22b1
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2020-06-29 17:32:38 +0200

    fix(account.move): checked if receivable is set

--- school_finance/models/account_move.py
+++ school_finance/models/account_move.py
@@ -10,12 +10,12 @@ class AccountMove(models.Model):
 
     family_members_ids = fields.Many2many(related="family_id.member_ids")
 
-    receivable_account_id = fields.Many2one("account.account", string="Receivable account")
+    receivable_account_id = fields.Many2one("account.account", string="Receivable account", domain=[("user_type_id.type", "=", "receivable")])
 
     def set_receivable_account(self):
         for record in self:
             receivable_line_id = record.line_ids.filtered(lambda record: record.account_id.user_type_id.type == 'receivable')
             receivable_line_id.ensure_one()
-            if (receivable_line_id):
+            if (receivable_line_id and record.receivable_account_id):
                 receivable_line_id.account_id = record.receivable_account_id.id
 
