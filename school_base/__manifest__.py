6f87ed030f90df70f255ed09ccea20e74edbaf71
LuisAngelMalaveMora@gmail.com <Zelda220402>
2020-07-07 11:34:50 +0200

    update

--- school_base/__manifest__.py
+++ school_base/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Administration',
-    'version': '0.4',
+    'version': '0.5',
 
     # any module necessary for this one to work correctly
     'depends': [
@@ -29,6 +29,8 @@
         'security/ir.model.access.csv',
         'data/menudata.xml',
         'views/res_partner_view.xml',
+        'views/res_company.xml',
+
         'views/portal_views.xml',
         'views/views.xml'
     ],
---
+++ school_base/models/res_company.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+from ..utils import formatting
+
+from odoo import fields, models, api, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+
+
+class Company(models.Model):
+    _inherit = "res.company"
+
+    district_code_id = fields.Many2one("school_base.district_code", "District code")
