cadfc6b58214a853917d5cde9909fd7b778c43c3
rainierking <rainierking@gitlab.com>
2020-10-29 21:44:18 +0800

    [ADD] adm_access: Add user group that can only access admissions module

---
+++ adm_access/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Admissions Access Rights',
+
+    'summary': """ Admissions Access Rights """,
+
+    'description': """
+        Admissions Access Rights
+    """,
+
+    'author': 'Eduwebgroup',
+    'website': 'http://www.eduwebgroup.com',
+
+    'category': 'Extra Tools',
+    'version': '1.0',
+
+    'depends': [
+        'adm',
+        'hide_menu_by_group',
+    ],
+
+    'data': [
+        'data/res_groups_data.xml',
+    ],
+}
