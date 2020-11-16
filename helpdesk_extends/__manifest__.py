903a31dd039a4f782f72d5fda1290f3d7c46c846
rainierking <rainierking@gitlab.com>
2020-11-16 11:27:12 +0800

    [ADD] helpdesk_extends: Add team members as followers and send them an email after ticket creation

---
+++ helpdesk_extends/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Helpdesk Extensions',
+
+    'summary': """ Helpdesk Extensions """,
+
+    'description': """
+        Helpdesk Extensions
+    """,
+
+    'author': 'Eduwebgroup',
+    'website': 'http://www.eduwebgroup.com',
+
+    'category': 'Helpdesk',
+    'version': '1.0',
+
+    'depends': [
+        'helpdesk',
+    ],
+
+    'data': [
+        'data/mail_template_data.xml',
+        'data/base_automation_data.xml',
+    ],
+}
